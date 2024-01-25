7cd5fed46150e7919ac92ae37739d8487f82fd24
SauravKumarLal <sauravkrlal@gmail.com>
2024-01-25 23:15:24 +0530

    first commit

---
+++ config/cloudinary.js
@@ -0,0 +1,3 @@
+//cloudinary k config method ko use kar k hum kar sakte hai cloudinary k saath connect
+
+//start from 30:00
---
+++ config/database.js
@@ -0,0 +1,17 @@
+const { error } = require("console");
+const mongoose = require("mongoose");
+
+require("dotenv").config();
+
+exports.connect = () => {
+    mongoose.connect(process.env.MONGODB_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(console.log("DB Connection Successful!"))
+    .catch( (error) => {
+        console.log("DB Connection Issues");
+        console.error(error);
+        process.exit(1); //status 1 means exit kar jana
+    });
+};
